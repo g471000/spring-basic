00b7ea318abc4951f4e0a0675556210406ff21fd
JIEUN CHON <g471000@gmail.com>
2021-12-25 22:05:37 +0900

    time tracker for some methods

--- src/main/java/com/treeeye/hellospring/service/MemberService.java
+++ src/main/java/com/treeeye/hellospring/service/MemberService.java
@@ -17,13 +17,27 @@ public class MemberService {
     }
 
     public Long join(Member member) {
-        validateDuplicateMember(member);
-        repository.save(member);
-        return member.getId();
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member);
+            repository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     public List<Member> findMembers() {
-        return repository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return repository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId) {
